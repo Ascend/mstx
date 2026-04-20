9845f48ed94c2ce32c925562468a7437f3d5bd24
h-glue <huangguang9@h-partners.com>
2026-04-20 20:27:30 +0800

    [bugfix]: 固定url为源码链接
    Co-authored-by: h-glue<huangguang9@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !29 merge url_branch into master
    
    [bugfix]: 固定url为源码链接
    
    Created-by: h-glue
    Commit-by: h-glue
    Merged-by: ascend-robot
    Description: ### 1. 修改描述
    - **修改原因：**  
    url为安装用户本地的远程仓可能会导致一些显示问题，比如账户密码原文暴露
    - **修改方案：**
    固定url为源码链接
    - **修改内容：**
    setup.py中固定url为源码链接
    - [ ] **涉及代码双合**(贴上另一个PR链接)：
    ----
    
    ### 2. 功能验证
    - [ ] **功能自验截图**（请确保不体现个人信息）  
    ![image.png](https://raw.gitcode.com/user-images/assets/8858750/7b598a1f-5f60-4c05-a90b-4945a127b1ba/image.png 'image.png')
    - [ ] **冒烟是否通过**  
    不涉及
    ----
    
    ### 3. 代码检视
    - **要求：**
      - 合入功能代码大于 200 行，需要sig会议申报代码检视议题，并在PR中标注会议。
      - committer评估是否需要在sig会议进行代码检视。
      - 参与检视的committer人员名单与检视时间。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**  
    - [ ] **是否具备UT测试用例看护**
    - [ ] **是否需要在sig会议中进行代码检视**
    - **检视committer人员名单与检视时间：**
    ----
    ### 4. 资料修改自检
    - **资料修改：**    
    ----
    
    See merge request: Ascend/mstx!29

--- setup.py
+++ setup.py
@@ -21,32 +21,6 @@ class CustomInstall(install):
         rel_src = os.path.relpath(src_abs, install_dir)
         os.symlink(rel_src, dst)
 
-def get_git_url():
-    url = 'https://gitcode.com/Ascend/mstx'
-    try:
-        # 获取远程 origin 的 URL
-        result = subprocess.run(
-            ["git", "config", "--get", "remote.origin.url"],
-            capture_output=True,
-            text=True,
-            check=False
-        )
-        if result.returncode == 0:
-            url = result.stdout.strip()
-            # 处理 SSH 地址（如 git@github.com:user/repo.git） → 转为 HTTPS
-            if url.startswith("ssh://"):
-                url = url.replace("ssh://", "https://").replace(":", "/", 1)
-            if url.startswith("git@"):
-                url = url.replace("git@", "https://").replace(":", "/", 1)
-            re.sub(r':\d+', '', url)
-            return url
-        else:
-            print("无法获取 Git URL，使用默认值")
-            return url
-    except Exception as e:
-        print("无法自动获取 Git URL，使用默认值")
-        return url
-
 setup(
     name = 'mstx',
     version = os.environ.get('WHL_VERSION', '26.0.0'),
@@ -55,7 +29,7 @@ setup(
     description = 'mstx',
     long_description = open('README.md', encoding='utf-8').read(),
     long_description_content_type = 'text/markdown',
-    url = get_git_url(),
+    url = 'https://gitcode.com/Ascend/mstx',
     options={
         'bdist_wheel':{
             'plat_name': tag.platform}},
